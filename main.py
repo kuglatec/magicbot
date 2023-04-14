99cc43575806ac5adfb6bba26414539fe6641dd3
Kuglatec <95873978+kuglatec@users.noreply.github.com>
2023-04-14 23:03:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 import discord
+import asyncio
+import requests
+import json
+import random
+
+intents = discord.Intents.default()
+intents.message_content = True
+client = discord.Client(intents = discord.Intents.all(), command_prefix='$')
+
+@client.event
+async def on_ready():
+    print('Successfully logged in')
+
+    
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
